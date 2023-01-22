acfb585220f0f31a0b549d6a49bdaa6141c3930f
Tatyana <Tatyanakayan@mail.ru>
2023-01-22 14:59:40 +0300

    программа считает сколь чисел больше 0

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int m = new Random().Next(1, 10);
+Console.WriteLine ($"Введите {m} полижительных и отрицательных чисел");
+int[] numbers = new int [m];
+
+Console.WriteLine(creatArray(numbers));
+Console.WriteLine($"В массиве {minusSum(numbers)} отрицательных чисел");
+
+int [] creatArray (int [] array){
+  for (int i = 0; i < array.Length; i++){
+    Console.WriteLine ($"Введите число");
+    array [i] = Convert.ToInt32(Console.ReadLine());
+  }
+return array;
+}
+
+int minusSum(int [] array){
+  int count = 0;  
+  foreach(int a in array){
+    if (a < 0) count++;
+  }return count;
+}
